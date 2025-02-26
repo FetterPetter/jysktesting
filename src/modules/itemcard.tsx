36e7a95b010805d762fccb3404d327183d939f99
FetterPetter <petterskjorten@gmail.com>
2025-02-26 10:48:47 +0100

    basic setup1

---
+++ src/modules/itemcard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Item } from "./data";
+
+interface ItemCardProps {
+  item: Item;
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
+  return (
+    <div className="item-card">
+      <img src={item.image} alt={item.name} />
+      <h3>{item.name}</h3>
+      <ul>
+        {Object.keys(item.details).map((key) => (
+          <li key={key}>
+            <strong>{key}:</strong> {item.details[key]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemCard;
