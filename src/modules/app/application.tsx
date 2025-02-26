30a952b2c04728f82c1150d4694869b3251f1511
FetterPetter <petterskjorten@gmail.com>
2025-02-26 10:02:28 +0100

    basic setup1

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { ButtonHandler } from "../buttonHandler";
 
 export function Application() {
   return (
     <>
       <h1>Hello</h1>
+      <ButtonHandler />
     </>
   );
 }
---
+++ src/modules/buttonHandler.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ButtonHandler() {
+  return <button>Hello</button>;
+}
